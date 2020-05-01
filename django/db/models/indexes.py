4a805fa28f46c07fc63b74d1e5e8c288d9e1312c
Hannes Ljungberg <hannes@5monkeys.se>
2020-05-01 22:46:30 +0200

    wip

--- django/db/models/indexes.py
+++ django/db/models/indexes.py
@@ -17,7 +17,7 @@ class Index:
 
     def __init__(
         self,
-        *,
+        *expressions,
         fields=(),
         name=None,
         db_tablespace=None,
@@ -37,16 +37,17 @@ class Index:
             raise ValueError('Index.opclasses must be a list or tuple.')
         if opclasses and len(fields) != len(opclasses):
             raise ValueError('Index.fields and Index.opclasses must have the same number of elements.')
-        if not fields:
-            raise ValueError('At least one field is required to define an index.')
         if include and not name:
             raise ValueError('A covering index must be named.')
-        if not isinstance(include, (type(None), list, tuple)):
-            raise ValueError('Index.include must be a list or tuple.')
-        if any(isinstance(field, (BaseExpression, F)) for field in fields) and not name:
-            raise ValueError('Index.name needs to be set when fields contain expressions.')
+        if not expressions and not fields:
+            raise ValueError('At least one field is required to define an index.')
+        if expressions and fields:
+            raise ValueError("'fields' cannot be used together with expressions.")
+        if expressions and not name:
+            raise ValueError('Index.name needs to be set when passed expressions.')
 
         self.fields = list(fields)
+        self.expressions = list(expressions)
         self.name = name or ''
         self.db_tablespace = db_tablespace
         self.opclasses = opclasses
@@ -66,17 +67,14 @@ class Index:
             field for field in self.fields if isinstance(field, (BaseExpression, F))
         ]
 
-        self.field_name_expressions = [
-            field_name
-            for field_name in self.fields
-            if isinstance(field_name, str)
-        ]
         self.field_names = [
             field_name.lstrip('-')
-            for field_name in self.field_name_expressions
+            for field_name in self.fields
         ]
-        self.expressions = [
-            field for field in self.fields if isinstance(field, (BaseExpression, F))
+
+        self._expressions = [
+            F(expression) if isinstance(expression, str) else expression
+            for expression in self.expressions
         ]
 
     def _get_condition_sql(self, model, schema_editor):
@@ -107,6 +105,15 @@ class Index:
         expression_sql = '(%s)' % expression_sql
         return expression_sql % params, suffix
 
+    def _get_expression_orders(self, model, schema_editor, using):
+        expressions = []
+        for expression in self._expressions:
+            expression_sql, order = self._get_ordered_expression(
+                model, schema_editor, using, expression
+            )
+            expressions.append((expression_sql, order))
+        return expressions
+
     def _get_ordered_field(self, model, field_name):
         """
         Return the field for supplied name and an optional ordering
@@ -117,18 +124,13 @@ class Index:
         field = model._meta.get_field(field_name)
         return field, order
 
-    def _get_field_orders(self, model, schema_editor, using):
+    def _get_field_orders(self, model):
         """
-        Return fields and expressions together with their ordering
+        Return fields together with their ordering
         """
         fields = []
         for field in self.fields:
-            if field in self.expressions:
-                field, order = self._get_ordered_expression(
-                    model, schema_editor, using, field
-                )
-            else:
-                field, order = self._get_ordered_field(model, field)
+            field, order = self._get_ordered_field(model, field)
             fields.append((field, order))
         return fields
 
@@ -138,7 +140,7 @@ class Index:
         """
         connection = schema_editor.connection
         supports_expression_indexes = connection.features.supports_expression_indexes
-        for column_expression in self.expressions:
+        for column_expression in self._expressions:
             if (
                 not supports_expression_indexes and
                 hasattr(column_expression, 'flatten') and
@@ -169,15 +171,17 @@ class Index:
             # everything else.
             warnings.warn(str(e), RuntimeWarning)
             return None
-        field_orders = self._get_field_orders(model, schema_editor, using)
-        fields = [field[0] for field in field_orders]
+        col_suffixes = fields = None
+        if self.fields:
+            field_orders = self._get_field_orders(model)
+            fields = [field[0] for field in field_orders]
+            col_suffixes = [order[1] for order in field_orders]
         include = [model._meta.get_field(field_name).column for field_name in self.include]
-        col_suffixes = [order[1] for order in field_orders]
         condition = self._get_condition_sql(model, schema_editor)
         return schema_editor._create_index_sql(
-            model, fields, name=self.name, using=using, db_tablespace=self.db_tablespace,
+            model, fields=fields, name=self.name, using=using, db_tablespace=self.db_tablespace,
             col_suffixes=col_suffixes, opclasses=self.opclasses, condition=condition,
-            include=include, **kwargs,
+            include=include, expressions=self._expressions, **kwargs,
         )
 
     def remove_sql(self, model, schema_editor, **kwargs):
@@ -186,6 +190,7 @@ class Index:
     def deconstruct(self):
         path = '%s.%s' % (self.__class__.__module__, self.__class__.__name__)
         path = path.replace('django.db.models.indexes', 'django.db.models')
+        # TODO: expressions
         kwargs = {'fields': self.fields, 'name': self.name}
         if self.db_tablespace is not None:
             kwargs['db_tablespace'] = self.db_tablespace
@@ -213,9 +218,8 @@ class Index:
         _, table_name = split_identifier(model._meta.db_table)
         column_names = [model._meta.get_field(field_name).column for field_name in self.field_names]
         column_names_with_order = [
-            (('-%s' if field_name_expresion.startswith('-') else '%s') % column_name)
-            for column_name, field_name_expresion in zip(
-                column_names, self.field_name_expressions
+            (('-%s' if field.startswith('-') else '%s') % column_name)
+            for column_name, field in zip(column_names, self.fields
             )
         ]
         # The length of the parts of the name is based on the default max
